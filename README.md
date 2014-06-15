12ff10652cd950f2a5800bdc3fd358a86e57d996
florianpnn <florian.peninon@gmail.com>
2014-06-15 11:55:21 +0200

    readme updated and file commented

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-sketch-plugin-updater-lib
+Sketch Plugin Updater Lib by Florian Peninon
 =========================
 
 A library to check if a Sketch plugin is up to date compare to its master version
+
+### Installation and usage
+
+Download check-update.js and copy/paste it into your plugin folder.
+The only step needed to make it work with your plugin is importing the file in your plugin like this :
+
+```
+// Beginning of your plugin..
+
+#import 'check-update.js'
+
+// Rest of your plugin
+```
+
+### How it works?
+
+The library analyze the number of character contained in both the remote and the local version of the plugin.
+If the length is different a modal shows up to inform you that a new version is available and give you the url where you can download it.
--- check-update.js
+++ check-update.js
@@ -1,21 +1,27 @@
 // Library to check new plugin update
-var pluginUrl = 'https://dl.dropboxusercontent.com/u/4822469/data-parser.sketchplugin';
+// Copyright 2014 Florian Peninon. All rights reserved.
+// This library use the number of character contained in each files to know if the local version of the plugin is the last one
+
+// Paste your master github raw version url here
+// Example : https://raw.githubusercontent.com/florianpnn/sketch-data-parser/master/sketch-data-parser.sketchplugin
+var pluginUrl = '';
+
+// Grab the master version of the plugin from the URL and store the exact length of the file
 var tmpPluginMaster = get(pluginUrl);
 var pluginMaster = NSString.alloc().initWithData_encoding(tmpPluginMaster, NSUTF8StringEncoding);
 var pluginMasterLength = pluginMaster.length();
 
+// Grab the local version and store the length
 var scriptPath = sketch.scriptPath;
-
 var currentPlugin = [NSString stringWithContentsOfFile:scriptPath encoding:NSUTF8StringEncoding error:nil];
 var currentPluginLength = currentPlugin.length();
 
-log(pluginMasterLength);
-log(currentPluginLength);
-
+// Show an alert if length are different
 if (pluginMasterLength != currentPluginLength) {
   alert('A new version of this plugin is available! Download it here : ' + pluginUrl,'New version available');
 }
 
+// Common function
 function get(url) {
   var request = NSURLRequest.requestWithURL(NSURL.URLWithString(url));
   var response = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, null, null);
